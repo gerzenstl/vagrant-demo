a2a069ad5768be61ac5c699e984c5d1aa44f4ea0
Luis Gerzenstein <luis@marvin.(none)>
2013-09-24 02:56:21 -0300

    Initial local commit

---
+++ src/drupal/puppet/manifests/default.pp
@@ -0,0 +1,189 @@
+$dbUsername = 'drupal_user'
+$dbUserPass = 'drupal_pass'
+
+
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    
+class { 'puphpet::dotfiles': }
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'drupal-via-vagrant.local':
+  server_name   => 'drupal-via-vagrant.local',
+  serveraliases => [],
+  docroot       => '/var/www/drupal',
+  port          => '80',
+  env_variables => [],
+  priority      => '1',
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-mysql': }
+php::module { 'php5-cli': }
+php::module { 'php5-common': }
+php::module { 'php5-curl': }
+php::module { 'php5-gd': }
+php::module { 'php5-mcrypt': }
+php::module { 'php-apc': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+$xhprofPath = '/var/www/xhprof'
+
+php::pecl::module { 'xhprof':
+  use_package     => false,
+  preferred_state => 'beta',
+}
+
+if !defined(Package['git-core']) {
+  package { 'git-core' : }
+}
+
+vcsrepo { $xhprofPath:
+  ensure   => present,
+  provider => git,
+  source   => 'https://github.com/facebook/xhprof.git',
+  require  => Package['git-core']
+}
+
+file { "${xhprofPath}/xhprof_html":
+  ensure  => 'directory',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  mode    => '0775',
+  require => Vcsrepo[$xhprofPath]
+}
+
+
+apache::vhost { 'xhprof':
+  server_name => 'xhprof',
+  docroot     => "${xhprofPath}/xhprof_html",
+  port        => 80,
+  priority    => '1',
+  require     => [
+    Php::Pecl::Module['xhprof'],
+    File["${xhprofPath}/xhprof_html"]
+  ]
+}
+
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000',
+    'xdebug.collect_params = 3',
+    'xdebug.collect_return = 1',
+    'xdebug.var_display_max_children = 100',
+    'xdebug.var_display_max_data = 8000',
+    'xdebug.var_display_max_depth = 15'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "America/Argentina/Buenos_Aires"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = On',
+    'memory_limit = 400M',
+    'html_errors = On'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+
+class { 'mysql::server':
+  config_hash   => { 'root_password' => 'root' }
+}
+
+mysql::db { 'drupal_vagrant':
+  grant    => [
+    'ALL'
+  ],
+  user     => $dbUsername,
+  password => $dbUserPass,
+  host     => 'localhost',
+  charset  => 'utf8',
+  require  => Class['mysql::server'],
+  sql       => '/vagrant/data/initial_data.sql'
+}
+
+#exec { 'Import site database'
+#  unless => "/usr/bin/mysql -u${dbUsername} -p${dbUserPass} drupal_vagrant",
+#  command => "/usr/bin/mysql -u${dbUsername} -p${dbUserPass} drupal_vagrant < " 
+#}
+
+class { 'phpmyadmin':
+  require => [Class['mysql::server'], Class['mysql::config'], Class['php']],
+}
+
+apache::vhost { 'phpmyadmin':
+  server_name => 'phpmyadmin',
+  docroot     => '/usr/share/phpmyadmin',
+  port        => 80,
+  priority    => '10',
+  require     => Class['phpmyadmin'],
+}
+
+
+
