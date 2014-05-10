592ae7799d9e1d8af8a80b8c052b27a5aa9bce76
Luis <gerzenstl@gmail.com>
2014-05-09 22:45:27 -0300

    Improved script provisioner

--- src/bash-simple-provisioner/setup.sh
+++ src/bash-simple-provisioner/setup.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-echo ">>> Starting provision"
+echo "            ██╗   ██╗ █████╗  ██████╗ ██████╗  █████╗ ███╗   ██╗████████╗                "
+echo "            ██║   ██║██╔══██╗██╔════╝ ██╔══██╗██╔══██╗████╗  ██║╚══██╔══╝                "
+echo "            ██║   ██║███████║██║  ███╗██████╔╝███████║██╔██╗ ██║   ██║                   "
+echo "            ╚██╗ ██╔╝██╔══██║██║   ██║██╔══██╗██╔══██║██║╚██╗██║   ██║                   "
+echo "             ╚████╔╝ ██║  ██║╚██████╔╝██║  ██║██║  ██║██║ ╚████║   ██║                   "
+echo "              ╚═══╝  ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝                   "
+echo "                                                                                         "
+echo "██████╗ ██████╗  ██████╗ ██╗   ██╗██╗███████╗██╗ ██████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗ "
+echo "██╔══██╗██╔══██╗██╔═══██╗██║   ██║██║██╔════╝██║██╔═══██╗████╗  ██║██║████╗  ██║██╔════╝ "
+echo "██████╔╝██████╔╝██║   ██║██║   ██║██║███████╗██║██║   ██║██╔██╗ ██║██║██╔██╗ ██║██║  ███╗"
+echo "██╔═══╝ ██╔══██╗██║   ██║╚██╗ ██╔╝██║╚════██║██║██║   ██║██║╚██╗██║██║██║╚██╗██║██║   ██║"
+echo "██║     ██║  ██║╚██████╔╝ ╚████╔╝ ██║███████║██║╚██████╔╝██║ ╚████║██║██║ ╚████║╚██████╔╝"
+echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝ "
+echo ""                                                                                        
+echo ""
+
 apt-get update
 
+# echo ">>> Installing PHP"
+# apt-get install -y -q php5 php5-cli php5-common php-pear php5-xdebug php5-mysql php5-gd php5-dev php-apc php5-curl
 
 echo ">>> Installing Apache Server"
-apt-get install -y apache2
+apt-get install -y apache2 libapache2-mod-php5
+/etc/init.d/apache2 restart
 
-echo ">>> Installing PHP"
-apt-get install -y -q php5-cli php5-common php-pear php5-xdebug php5-mysql php5-gd php5-dev php-apc php5-curl php-xml
+echo ">>> Installing MySQLr"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server
 
 echo ">>> Organizing folder structure"
 rm -rf /var/www
