9b0a52f68fbc25148724b46f6ab30a78efef262f
Luis Gerzenstein <gerzenstl@gmail.com>
2014-07-05 20:39:27 -0300

    Updated bash-simple-provisioner. Work in progress

--- src/bash-simple-provisioner/setup.sh
+++ src/bash-simple-provisioner/setup.sh
@@ -16,19 +16,26 @@ echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝   ╚═══╝
 echo ""                                                                                        
 echo ""
 
-apt-get update
+sudo apt-get update
 
-# echo ">>> Installing PHP"
-# apt-get install -y -q php5 php5-cli php5-common php-pear php5-xdebug php5-mysql php5-gd php5-dev php-apc php5-curl
+echo ">>> Installing PHP"
+sudo apt-get install -y -q php5 php5-cli php5-common php-pear php5-xdebug php5-mysql php5-gd php5-dev php-apc php5-curl
 
 echo ">>> Installing Apache Server"
-apt-get install -y apache2 libapache2-mod-php5
+sudo apt-get install -y apache2 libapache2-mod-php5
 /etc/init.d/apache2 restart
 
-echo ">>> Installing MySQLr"
+echo ">>> Installing MySQL"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -y install mysql-server
+
+echo ">>> Installing VIM LIKE A LOSER"
+# sudo apt-get install -y vim
+
+cd /var/www/
+mkdir test
+
 
 echo ">>> Organizing folder structure"
 rm -rf /var/www
